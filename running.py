7143dd210bbacf357f5c742991eca35141399d06
llzmgjzhy <guangzewei@foxmail.com>
2025-06-10 16:25:35 +0800

    v2.5: use mean cls as query. query cal cross-attn with all cls.

--- running.py
+++ running.py
@@ -400,7 +400,7 @@ class ClassificationRunner(BaseRunner):
         test_labels = np.concatenate(per_batch["targets"], axis=0).reshape(-1)
 
         pred = np.argmax(pred, axis=1)
-        # pred = (pred > 0.5).cpu().numpy().astype(int)
+        # pred = (torch.sigmoid(pred) > 0.5).cpu().numpy().astype(int)
         gt = np.array(test_labels).astype(int)
 
         accuracy = accuracy_score(gt, pred)
